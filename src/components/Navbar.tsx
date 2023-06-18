259175cf5305de788b5763ce4a6819a066ed6d13
Taylor Allen <taylorallen0913@gmail.com>
2023-06-18 02:12:59 -0700

    fix: fixed mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -74,7 +74,50 @@ const Navbar: React.FC = () => {
       >
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
-          {/* ... rest of the code ... */}
+          <div className="flex items-center justify-between">
+            <Link href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">iambusy</span>
+              <h1 className="font-satoshi text-xl font-extrabold text-white">
+                iambusy
+              </h1>
+            </Link>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-400"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/25">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-300 hover:bg-gray-800"
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+              <div>
+                <SignedIn>
+                  <UserButton afterSignOutUrl="/" />
+                </SignedIn>
+                <SignedOut>
+                  <Button
+                    href="/sign-in"
+                    title="Log in"
+                    variant="secondary"
+                    className="w-full text-center"
+                  />
+                </SignedOut>
+              </div>
+            </div>
+          </div>
         </Dialog.Panel>
       </Dialog>
     </header>
